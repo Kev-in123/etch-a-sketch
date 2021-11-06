2667b83b1a57b7a12258fb3ed02b657e01151a70
Kevin Cai <75402062+Kev-in123@users.noreply.github.com>
2021-11-06 13:44:48 -0400

    make indentation look nicer and add __main__ guard

--- main.py
+++ main.py
@@ -1,38 +1,42 @@
+
 import pygame
-pygame.init()
-win = pygame.display.set_mode((1500,750))
-white = (255, 255, 255)
-win.fill(white)
 
-x,y,vel = 750, 375, 1
+def main():
+    pygame.init()
+    win = pygame.display.set_mode((1500,750))
+    white = (255, 255, 255)
+    win.fill(white)
+    x, y, vel = 750, 375, 1
 
+    while True:
+        pygame.time.delay(10)
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:    
+              return
 
-run = True
-while run:
-  pygame.time.delay(10)
-  for event in pygame.event.get():
-    if event.type == pygame.QUIT:    
-      run = False
-
-  keys = pygame.key.get_pressed()
-  
-  if keys[pygame.K_LEFT] and x > 0:
-    x -= vel
-
-  if keys[pygame.K_RIGHT] and x < 1500-1:
-    x += vel
-  
-  if keys[pygame.K_UP] and y > 0:
-    y -= vel
-          
-  
-  if keys[pygame.K_DOWN] and y < 750-1:
-
-    y += vel
-         
-              
-  pygame.draw.rect(win, (0, 0, 0), (x, y, 1, 1))
+        keys = pygame.key.get_pressed()
+       
+        if keys[pygame.K_LEFT] and x > 0:
+          x -= vel
       
-  pygame.display.update() 
-  
-pygame.quit()
+        if keys[pygame.K_RIGHT] and x < 1500-1:
+          x += vel
+        
+        if keys[pygame.K_UP] and y > 0:
+          y -= vel
+                
+        
+        if keys[pygame.K_DOWN] and y < 750-1:
+      
+          y += vel
+              
+                    
+        pygame.draw.rect(win, (0, 0, 0), (x, y, 1, 1))
+            
+        pygame.display.update() 
+    
+    pygame.quit()
+
+
+if __name__ =='__main__':
+    main()
